4d3eeac7271dabe7b5013a8e1ad8617a67167f3d
Rafael Flores Souza <Raflso@icloud.com>
2022-09-09 21:33:23 -0600

    Starting files

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App() {
+  return <></>
+  // return <BrowserRouter>
+  //           <Routes>
+  //               <Route path="/register" element="" />
+  //               <Route path="/login" element="" />
+  //               <Route path="/" element="" />
+  //           </Routes>
+  //         </BrowserRouter>;
+}
+
+export default App;
