1af10c8c61a4c2ec9044f9c8089beffced3b683a
Mar Ibarra <43395194+kiwigi@users.noreply.github.com>
2022-12-10 13:20:41 -0700

    setting up the chat page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { FormType } from './components/Start/Form';
 import Start from './components/Start/Start';
 import { LoggedUserProvider } from './contexts/LoggesUserContext';
+import ChatBody from './components/Chat/ChatBody';
 
 function App() {
 
@@ -12,6 +13,8 @@ function App() {
               <Route path="/" element={<Start formType={FormType.Registration}></Start>} />
               <Route path="/register" element={<Start formType={FormType.Registration}></Start>} />
               <Route path="/login" element={<Start formType={FormType.Login}></Start>} />
+              <Route path="/chat" element={<ChatBody />} />
+
             </Routes>
           </LoggedUserProvider>    
         </BrowserRouter>;
---
+++ src/components/Chat/ChatBody.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react'
+import styled from 'styled-components';
+
+
+export default function ChatBody(){
+    return(
+        <>
+        PENIS
+        </>
+    );
+
+
+}
+
