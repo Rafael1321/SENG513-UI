5a66fe2a38ea5694bd9213247ffb1727a6f2a1f2
Mar Ibarra <43395194+kiwigi@users.noreply.github.com>
2022-12-10 17:31:58 -0700

    profile card is mostly finished, stilll need to make it responsive and fix spacing issues

--- src/components/Chat/ChatBody.tsx
+++ src/components/Chat/ChatBody.tsx
@@ -1,14 +1,11 @@
-import * as React from 'react'
-import styled from 'styled-components';
+import * as React from "react";
+import styled from "styled-components";
+import ProfileCard from "./ProfileCard";
 
-
-export default function ChatBody(){
-    return(
-        <>
-        PENIS
-        </>
-    );
-
-
+export default function ChatBody() {
+  return (
+    <>
+      <ProfileCard imgSrc="/assets/jettFurry.png" userName="IMNOTAFURRY" basicInfo="22F, US West" userType="Competitive" valRank="assets/d2.png" chatRank="/assets/ToxicWaste.png" aboutMe="I used to be Immortal but I got a bunch of dog water teammates and demoted to diamond. IF YOU’RE DOG WATER GO NEXT! DON’T WASTE MY TIME I’M GONNA BE PRO I DONT NEED LIL DOGGIES WITH THEIR TAILS BETWEEN THEIR LEGS DRAGGING ME DOWN!!!!grrr meow :3"/>
+    </>
+  );
 }
-
---
+++ src/components/Chat/ProfileCard.tsx
@@ -0,0 +1,108 @@
+import * as React from "react";
+import styled from "styled-components";
+
+interface Props {
+  imgSrc: string;
+  userName: string;
+  valRank?: string;
+  chatRank: string;
+  basicInfo?: string;
+  userType: string;
+  aboutMe?: string;
+}
+
+export default function ProfileCard(
+  props: Props
+): React.ReactElement<Props, any> {
+  return (
+    <>
+      <Wrapper>
+        <Icon imgSrc={props.imgSrc} />
+        <Username>{props.userName}</Username>
+        <BasicInfo>{props.basicInfo}</BasicInfo>
+        <BasicInfo>{props.userType}</BasicInfo>
+        <Ranks>
+          <RankImg imgSrc={props.valRank} />
+          <RankImg imgSrc={props.chatRank} />
+        </Ranks>
+        <Ranks>
+          <Label style={{ textAlign: "center" }}>RANK</Label>
+          <Label>REPUTATION</Label>
+        </Ranks>
+        <Label>ABOUT ME:</Label>
+        <AboutMe>{props.aboutMe}</AboutMe>
+      </Wrapper>
+    </>
+  );
+}
+
+const Wrapper = styled("div")`
+  background-color: #282828;
+  float: right;
+  width: 27vw;
+  max-width: 400px;
+  height: 70vh;
+  padding: 5vh;
+  border-radius: 44px;
+  filter: drop-shadow(0px 0px 10px #66c2a9);
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  @media all and (max-width: 900px){
+    flex-direction:row;
+    width: 80vw;
+    height:3vh;
+    padding: 2vh;
+    max-width: 700px
+    max-height: 200px;
+  }
+`;
+
+const Icon = styled.img<{ imgSrc: string }>`
+  content: url(${(props) => props.imgSrc});
+  border-radius: 50%;
+  width: 13vw;
+  max-width: 200px;
+  max-height: 200px;
+  aspect-ratio: 1;
+  margin-left: auto;
+  margin-right: auto;
+  display: block;
+`;
+const Username = styled.p`
+  text-align: center;
+  font-size: 2vw;
+  font-weight: 600;
+`;
+const BasicInfo = styled.p`
+  text-align: center;
+  font-size: 1vw;
+  font-weight: 400;
+`;
+const Ranks = styled.div`
+  display: flex;
+  width: 60%;
+  margin-left: auto;
+  margin-right: auto;
+  justify-content: space-between;
+`;
+const RankImg = styled.img<{ imgSrc: string }>`
+  content: url(${(props) => props.imgSrc});
+  justify-content: center;
+  width: 5vw;
+  max-width: 54px;
+  height: 5vw;
+  max-height: 54px;
+`;
+
+const AboutMe = styled.p`
+  text-align: left;
+  font-size: 1vw;
+  font-weight: 400;
+  font-family: "Arimo", sans-serif;
+`;
+const Label = styled.p`
+  text-align: left;
+  font-size: 1.2vw;
+  font-weight: 600;
+`;
