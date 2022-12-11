da2d1df5f1c0b06fe22870f9e802f745e0503f86
Mar Ibarra <43395194+kiwigi@users.noreply.github.com>
2022-12-10 19:17:29 -0700

    profile card is now responsive

--- src/components/Chat/ProfileCard.tsx
+++ src/components/Chat/ProfileCard.tsx
@@ -1,3 +1,4 @@
+import { style } from "@mui/system";
 import * as React from "react";
 import styled from "styled-components";
 
@@ -20,17 +21,20 @@ export default function ProfileCard(
         <Icon imgSrc={props.imgSrc} />
         <Username>{props.userName}</Username>
         <BasicInfo>{props.basicInfo}</BasicInfo>
-        <BasicInfo>{props.userType}</BasicInfo>
         <Ranks>
-          <RankImg imgSrc={props.valRank} />
-          <RankImg imgSrc={props.chatRank} />
+          <RankLabel>
+            <RankImg imgSrc={props.valRank} />
+            RANK
+          </RankLabel>
+          <RankLabel style={{ textAlign: "center" }}>
+            <RankImg imgSrc={props.chatRank} />
+            REPUTATION
+          </RankLabel>
         </Ranks>
-        <Ranks>
-          <Label style={{ textAlign: "center" }}>RANK</Label>
-          <Label>REPUTATION</Label>
-        </Ranks>
-        <Label>ABOUT ME:</Label>
-        <AboutMe>{props.aboutMe}</AboutMe>
+        <AboutContainer>
+          <Label>ABOUT ME:</Label>
+          <AboutMe>{props.aboutMe}</AboutMe>
+        </AboutContainer>
       </Wrapper>
     </>
   );
@@ -47,14 +51,14 @@ const Wrapper = styled("div")`
   display: flex;
   flex-direction: column;
   flex-wrap: wrap;
-  @media all and (max-width: 900px){
-    // flex-direction:row;
-    width: 80vw;
-    height:20vh;
+  @media all and (max-width: 800px) {
+    width: 90vw;
+    max-width: 90%;
+    height: 13vh;
     padding: 2vh;
-   
+    filter: drop-shadow(0px 0px 5px #66c2a9);
     max-height: 200px;
-    border-radius: 20px; 
+    border-radius: 20px;
   }
 `;
 
@@ -68,21 +72,18 @@ const Icon = styled.img<{ imgSrc: string }>`
   margin-left: auto;
   margin-right: auto;
   display: block;
-  @media all and (max-width: 900px){
-    width: 5vw;
-    height: 5vw;
-
-  }
 `;
 const Username = styled.p`
   text-align: center;
-  font-size: 2vw;
+  font-size: min(3vw, 35px);
+  margin: 5px;
   font-weight: 600;
 `;
 const BasicInfo = styled.p`
   text-align: center;
-  font-size: 1vw;
+  font-size: min(15px, 2vw);
   font-weight: 400;
+  margin: 0;
 `;
 const Ranks = styled.div`
   display: flex;
@@ -90,24 +91,57 @@ const Ranks = styled.div`
   margin-left: auto;
   margin-right: auto;
   justify-content: space-between;
+  @media all and (max-width: 800px) {
+    flex-direction: column;
+    width: 10%;
+    margin-top: auto;
+    margin-bottom: auto;
+  }
 `;
 const RankImg = styled.img<{ imgSrc: string }>`
   content: url(${(props) => props.imgSrc});
   justify-content: center;
+  margin-left: auto;
+  margin-right: auto;
   width: 5vw;
   max-width: 54px;
   height: 5vw;
   max-height: 54px;
 `;
 
-const AboutMe = styled.p`
+const AboutContainer = styled.div`
   text-align: left;
-  font-size: 1vw;
+  overflow: scroll;
+  margin-left: auto;
+  margin-right: auto;
+  @media all and (max-width: 800px) {
+    width: 40%;
+    height: 90%;
+  }
+`;
+
+const AboutMe = styled.p`
+  font-size: min(3vw, 20px);
   font-weight: 400;
   font-family: "Arimo", sans-serif;
+  margin: 0;
 `;
 const Label = styled.p`
   text-align: left;
-  font-size: 1.2vw;
+  font-size: min(20px, 2vw);
   font-weight: 600;
+
+  @media all and (max-width: 800) {
+    visibility: hidden;
+    margin: 0;
+  }
+`;
+const RankLabel = styled(Label)`
+  text-align: center;
+  font-size: min(20px, 1.5vw);
+  display: flex;
+  flex-direction: column;
+  @media all and (max-width: 800) {
+    max-width: 54px;
+  }
 `;
