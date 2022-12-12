11a2b275799663b6df381a2ce1f55362e737e561
Rafael Flores Souza <Raflso@icloud.com>
2022-12-11 17:23:53 -0700

    Added matching service and search by id for auth service

--- src/util/ApiConfig.ts
+++ src/util/ApiConfig.ts
@@ -5,12 +5,16 @@ export class ApiConfig{
 
     private static baseRoute : string = `http://${EnvConfig.API_HOST}:${EnvConfig.API_PORT}/`;
 
-    /* Start */
+    /* User */
     public static loginRoute = () => this.baseRoute + `users/login`;
     public static registerRoute = () => this.baseRoute + `users/register`;
     public static updateUserRoute = () => this.baseRoute + `users/update`;
+    public static findUserRoute = (userId : string) => this.baseRoute + `users/${userId}`;
 
     /* Filters */
     public static upsertFiltersRoute = () => this.baseRoute + `filters`;
     public static retrieveFiltersRoute = (retrieveDTO : IRetrieveDTO) => this.baseRoute + `filters/${retrieveDTO.userId}`;
+
+    /* Matching */ 
+    public static findMatchRoute = (userId : string) => this.baseRoute + `matchings/${userId}`
 }
