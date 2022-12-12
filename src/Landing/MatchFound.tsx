3056a85665da50a13d0215c4a1096f8ea83d5580
Harkamal Randhawa <harkamalsr@gmail.com>
2022-12-11 19:12:13 -0700

    Finished the countdown?

--- src/Landing/MatchFound.tsx
+++ src/Landing/MatchFound.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import styled from "styled-components";
 import { useState, useEffect } from "react";
 
+
 export default function MatchFound(props: any) {
   const [countdown, setCountdown] = React.useState(3);
 
@@ -22,28 +23,60 @@ export default function MatchFound(props: any) {
   return (
     <div>
       {countdown > 0 ? (
-        // If the countdown is greater than 0, render the countdown
-        // in a red, centered, font-size 100px element.
-        <div
-          style={{
-            color: 'red',
-            fontSize: '10vw', // Use vw units for the font size to make it responsive
-            textAlign: 'center',
-            position: 'absolute',
-            top: '50%',
-            left: '50%',
-            transform: 'translate(-50%, -50%)',
-            width: '50vw', // Use vw units for the element width to make it responsive
-            height: '50vh', // Use vh units for the element height to make it responsive
-          }}
-        >
-          {"Match Found!\n"+countdown}
-        </div>
+        countdown === 3 ? (
+          <div
+            style={{
+              color: 'white',
+              fontSize: '2.5vw',
+              textAlign: 'center',
+              position: 'absolute',
+              top: '30%',
+              left: '50%',
+              transform: 'translate(-50%, -50%)',
+              width: '10vw',
+              height: '5vh',
+            }}
+          >
+            Match Found!
+          </div>
+        ) : (
+          <div
+            style={{
+              color: 'red',
+              fontSize: '10vw',
+              textAlign: 'center',
+              position: 'absolute',
+              top: '50%',
+              left: '50%',
+              transform: 'translate(-50%, -50%)',
+              width: '50vw',
+              height: '50vh',
+            }}
+          >
+            {countdown}
+          </div>
+        )
       ) : (
-        // If the countdown is 0 or less, don't render anything.
-        <div />
+        countdown === 0 ? (
+          <div
+            style={{
+              color: 'white',
+              fontSize: '3vw',
+              textAlign: 'center',
+              position: 'absolute',
+              top: '30%',
+              left: '50%',
+              transform: 'translate(-50%, -50%)',
+              width: '10vw',
+              height: '10vh',
+            }}
+          >
+            Match has started!
+          </div>
+        ) : (
+          <div />
+        )
       )}
-      <FindDuoText>Match Found!</FindDuoText>
       <FindDuoContainer>
         <Teammate icon={props.imgSrc}>
           <p>{props.name}</p>
@@ -51,48 +84,10 @@ export default function MatchFound(props: any) {
         <Teammate icon={"Images/Icons/Astra_icon.webp"}>
           <p>Astra</p>
         </Teammate>
-      </FindDuoContainer>
-    </div>
-  );
-}/*
-export default function MatchFound(props: any) {
-
-  // Red text at the center of the screen which counts down from 3 to 0
-  var countdown = document.createElement('countdown');
-  countdown.style.color = 'red';
-  countdown.style.fontSize = '100px';
-  countdown.style.textAlign = 'center';
-  countdown.style.position = 'absolute';
-  countdown.style.top = '50%';
-  countdown.style.left = '50%';
-  countdown.style.transform = 'translate(-50%, -50%)';
-  document.getElementsByName('div')[0].appendChild(countdown);
-
-  var count = 3;
-  var countdownInterval = setInterval(function() {
-    countdown.innerHTML = ""+count;
-    count--;
-    if (count < 0) {
-      clearInterval(countdownInterval);
-      countdown.innerHTML = '';
-    }
-  }, 2000);
-
-  return(
-    <div>
-    <FindDuoText>Match Found!</FindDuoText>
-    <FindDuoContainer>
-      <Teammate icon={props.imgSrc}>
-        <p>{props.name}</p>
-      </Teammate>
-      <Teammate icon={"Images/Icons/Astra_icon.webp"}>
-        <p>Astra</p>
-      </Teammate>
     </FindDuoContainer>
-    </div>
-  );
+  </div>
+);
 }
-*/
 
 const Teammate = styled.div<{ icon: string }>`
   min-width: 9rem;
@@ -125,16 +120,6 @@ const Teammate = styled.div<{ icon: string }>`
     min-height: 7rem;
     min-width: 7rem;
   }
-`;
-
-const FindDuoText = styled.div`
-  min-width: 9rem;
-  min-height: 9rem;
-
-
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
 `;
 
 const FindDuoContainer = styled.div`
