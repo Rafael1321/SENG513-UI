aef23f421d39663e5904e1d372e9a90da962e97b
ArcticGlacier <sweetpapaya001@gmail.com>
2022-12-13 14:35:20 -0700

    Redid Alignment for MatchFound

--- src/components/Landing/MatchFound.tsx
+++ src/components/Landing/MatchFound.tsx
@@ -2,12 +2,11 @@ import * as React from "react";
 import styled from "styled-components";
 import { useState, useEffect, useContext } from "react";
 import { useNavigate } from "react-router-dom";
-import { LoggedUserContext } from '../../contexts/LoggedUserContext';
-import { MatchedUserContext } from '../../contexts/MatchedUserContext';
+import { LoggedUserContext } from "../../contexts/LoggedUserContext";
+import { MatchedUserContext } from "../../contexts/MatchedUserContext";
 
 export default function MatchFound() {
-  
-  // Contexts 
+  // Contexts
   const loggedUserContext = useContext(LoggedUserContext);
   const matchedUserContext = useContext(MatchedUserContext);
 
@@ -18,7 +17,6 @@ export default function MatchFound() {
   const navigate = useNavigate();
 
   useEffect(() => {
-    
     if (countdown > 0) {
       // Use setTimeout to schedule an update to the countdown state
       // every 1 second. When the countdown reaches 0, clear the
@@ -29,47 +27,73 @@ export default function MatchFound() {
       return () => {
         clearTimeout(timeout);
       };
-    }else{
+    } else {
       setTimeout(() => {
-        navigate('../chat');
+        navigate("../chat");
       }, 1000);
     }
   }, [countdown]);
 
-  return (<>
-      <MatchFoundText>Match Found</MatchFoundText>
-      <FindDuoContainer>
-        <Teammate icon={loggedUserContext?.loggedUser?.avatarImage}>
-          <p>{loggedUserContext?.loggedUser?.displayName ?? '<username 1>'}</p>
-        </Teammate>
-        <CountDownText>{countdown}</CountDownText>
-        <Teammate icon={matchedUserContext?.matchedUser?.avatarImage}>
-          <p>{matchedUserContext?.matchedUser?.displayName ?? '<username 2>'}</p>
-        </Teammate>
-      </FindDuoContainer>
-    </>);
+  return (
+    <>
+      <MatchContainer>
+        <MatchFoundText>Match Found</MatchFoundText>
+        <FindDuoContainer>
+          <ProfileDiv>
+            <Teammate
+              icon={loggedUserContext?.loggedUser?.avatarImage}
+            ></Teammate>
+            <p>
+              {loggedUserContext?.loggedUser?.displayName ?? "<username 1>"}
+            </p>
+          </ProfileDiv>
+
+          <CountDownText>{countdown}</CountDownText>
+          <ProfileDiv>
+            <Teammate
+              icon={matchedUserContext?.matchedUser?.avatarImage}
+            ></Teammate>
+            <p>
+              {matchedUserContext?.matchedUser?.displayName ?? "<username 2>"}
+            </p>
+          </ProfileDiv>
+        </FindDuoContainer>
+      </MatchContainer>
+    </>
+  );
 }
 
+const MatchContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+`;
+
+const ProfileDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+`;
+
 const MatchFoundText = styled.p`
-  font-family: 'valorant';
+  font-family: "valorant";
   font-size: 2.8vw;
   margin: 0;
   padding: 0vw 0;
 `;
 
 const CountDownText = styled.p`
-  color: #F94B4B;
+  color: #f94b4b;
   font-size: 5vw;
   width: 5vw;
-  font-family: 'valorant';
+  font-family: "valorant";
 `;
 
-const Teammate = styled.div<{ icon: string }>`
+const Teammate = styled.img<{ icon: string }>`
   min-width: 9rem;
   min-height: 9rem;
 
-  background: linear-gradient(rgba(0, 0, 0, 1), rgba(8, 71, 50, 0.2)),
-    url(${(props) => props.icon});
+  background: url(${(props) => props.icon});
 
   background-size: contain;
   background-repeat: no-repeat;
@@ -78,43 +102,26 @@ const Teammate = styled.div<{ icon: string }>`
   flex-direction: column;
   justify-content: center;
   border-color: rgb(102, 194, 169, 0.5);
+  border-radius: 50%;
+  border: 5px solid #66c2a9;
+  height: 9rem;
+  width: 9rem;
+  z-index: 4;
+  transition: all 0.5s ease-in-out;
+  background-color: #266152;
 
-  & #question-mark {
-    font-size: 5rem;
-    font-weight: 600;
-
-    color: #c3c3c3;
-    z-index: 6;
+  @media (max-width: 1025px) {
+    height: 8rem;
+    width: 8rem;
   }
 `;
 
 const FindDuoContainer = styled.div`
   display: flex;
   flex-direction: row;
-  justify-content: space-evenly;
+  justify-content: space-around;
   align-items: center;
   padding: 0 15%;
-
-  & img,
-    div {
-      border-radius: 50%;
-      border: 5px solid #66c2a9;
-      height: 9rem;
-      width: 9rem;
-      z-index: 4;
-      transition: all 0.5s ease-in-out;
-      background-color: #266152;
-
-      @media (max-width: 1025px) {
-        height: 8rem;
-        width: 8rem;
-      }
-
-      @media (max-width: 480px) {
-        height: 7rem;
-        width: 7rem;
-      }
-    }
 
   @media (max-width: 1025px) {
     justify-content: space-between;
